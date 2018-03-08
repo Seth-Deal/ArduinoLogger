b68696c9b5a9cffccd57cdaf43c888d9251272bc
Seth Deal <37190139+Seth-Deal@users.noreply.github.com>
2018-03-08 14:08:25 -0500

    Comments

--- AppartmentSensor/AppartmentSensor.ino
+++ AppartmentSensor/AppartmentSensor.ino
@@ -59,8 +59,10 @@ float ReadTempF(int sensor) {
 	// converting that reading to voltage, for 3.3v arduino use 3.3
 	float voltage = reading * 5.0;
 	voltage /= 1024.0;
-	float temperatureC = (voltage - 0.5) * 100; //converting from 10 mv per degree wit 500 mV offset
-												//to degrees ((voltage - 500mV) times 100)
+	float temperatureC = (voltage - 0.5) * 100;
+	//converting from 10 mv per degree wit 500 mV offset
+	//to degrees ((voltage - 500mV) times 100)
+
 	// now convert to Fahrenheit
 	float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
 	return temperatureF;
